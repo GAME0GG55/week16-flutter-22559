b5892cea416ff91c5097215dd2c217eac6e3449e
GAME0GG55 <itthekhnoloyisarsnthes92@gmail.com>
2026-02-13 13:07:19 +0700

    update login

---
+++ android/app/src/main/kotlin/com/example/flutter_application_weeek16/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_application_weeek16
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/view/auth/register.dart
@@ -0,0 +1,336 @@
+import 'package:flutter/material.dart';
+import 'login.dart';
+
+class RegisterScreen extends StatefulWidget {
+  const RegisterScreen({super.key});
+
+  @override
+  State<RegisterScreen> createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  bool showPass = false;
+  bool showPass2 = false;
+
+  final formKey = GlobalKey<FormState>();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final fullNameController = TextEditingController();
+  final confirmPasswordController = TextEditingController();
+
+  void register() {
+    if (formKey.currentState!.validate()) {
+      debugPrint(emailController.text);
+      debugPrint(passwordController.text);
+      debugPrint(fullNameController.text);
+      debugPrint(confirmPasswordController.text);
+    } else {
+      debugPrint('Have error in form');
+    }
+  }
+
+  bool isEmailValid(String email) {
+    RegExp regex = RegExp(
+      r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+",
+    );
+    return regex.hasMatch(email);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Container(
+            padding: const EdgeInsets.all(8.0),
+            alignment: Alignment.center,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                //===== Image Regsiter =====
+                Image.asset(
+                  'assets/images/register.png',
+                  width: size.width * 0.65,
+                ),
+                //===== Text Field Register =====
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Row(
+                    children: [
+                      Text(
+                        'Register',
+                        style: TextStyle(fontSize: 30.0, color: Colors.purple),
+                      ),
+                    ],
+                  ),
+                ),
+                //===== Form Register =====
+                Form(
+                  key: formKey,
+                  child: Column(
+                    children: [
+                      //===== Text Field Full Name =====
+                      TextFormField(
+                        controller: fullNameController,
+                        keyboardType: TextInputType.text,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            debugPrint('full name is empty');
+                            return 'full name is empty';
+                          } 
+                          return null;
+                        },
+                        decoration: InputDecoration(
+                          hintText: 'Enter your full name',
+                          label: Text("Full Name"),
+                          labelStyle: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 18.0,
+                            fontFamily: "verdana_regular",
+                            fontWeight: FontWeight.w400,
+                          ),
+                          prefixIcon: Icon(Icons.person),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5.0),
+                            borderSide: BorderSide(
+                              color: Colors.grey,
+                              width: 1.0,
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5.0),
+                            borderSide: BorderSide(
+                              color: Colors.grey,
+                              width: 1.0,
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 15.0),
+                       //===== Text Field Email =====
+                      TextFormField(
+                        controller: emailController,
+                        keyboardType: TextInputType.emailAddress,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            debugPrint('email is empty');
+                            return 'email is empty';
+                          } else if (!isEmailValid(value)) {
+                            debugPrint('email is invalid');
+                            return 'email is invalid';
+                          }
+                          return null;
+                        },
+                        decoration: InputDecoration(
+                          hintText: 'Enter your email or username',
+                          label: Text("Email or username"),
+                          labelStyle: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 18.0,
+                            fontFamily: "verdana_regular",
+                            fontWeight: FontWeight.w400,
+                          ),
+                          prefixIcon: Icon(Icons.email),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5.0),
+                            borderSide: BorderSide(
+                              color: Colors.grey,
+                              width: 1.0,
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5.0),
+                            borderSide: BorderSide(
+                              color: Colors.grey,
+                              width: 1.0,
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 15.0),
+                      //===== Text Field Password =====
+                      TextFormField(
+                        controller: passwordController,
+                        keyboardType: TextInputType.visiblePassword,
+                        obscureText: showPass ? false : true,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            debugPrint('password is empty');
+                            return 'password is empty';
+                          }
+                          return null;
+                        },
+                        decoration: InputDecoration(
+                          hintText: 'Enter your password',
+                          label: Text("Password"),
+                          labelStyle: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 18.0,
+                            fontFamily: "verdana_regular",
+                            fontWeight: FontWeight.w400,
+                          ),
+                          prefixIcon: Icon(Icons.vpn_key_sharp),
+                          suffixIcon: IconButton(
+                            onPressed: () {
+                              setState(() {
+                                showPass = !showPass;
+                              });
+                            },
+                            icon: Icon(
+                              showPass
+                                  ? Icons.visibility
+                                  : Icons.visibility_off,
+                            ),
+                          ),
+
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5.0),
+                            borderSide: BorderSide(
+                              color: Colors.grey,
+                              width: 1.0,
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5.0),
+                            borderSide: BorderSide(
+                              color: Colors.grey,
+                              width: 1.0,
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 15.0),
+                      //===== Text Field Confirm Password =====
+                      TextFormField(
+                        controller: confirmPasswordController,
+                        keyboardType: TextInputType.visiblePassword,
+                        obscureText: showPass2 ? false : true,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            debugPrint('confirm password is empty');
+                            return 'confirm password is empty';
+                          }
+                          if (value != passwordController.text) {
+                            debugPrint('passwords do not match');
+                            return 'passwords do not match';
+                          }
+                          return null;
+                        },
+                        decoration: InputDecoration(
+                          hintText: 'Enter your password',
+                          label: Text("Password"),
+                          labelStyle: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 18.0,
+                            fontFamily: "verdana_regular",
+                            fontWeight: FontWeight.w400,
+                          ),
+                          prefixIcon: Icon(Icons.vpn_key_sharp),
+                          suffixIcon: IconButton(
+                            onPressed: () {
+                              setState(() {
+                                showPass2 = !showPass2;
+                              });
+                            },
+                            icon: Icon(
+                              showPass2
+                                  ? Icons.visibility
+                                  : Icons.visibility_off,
+                            ),
+                          ),
+
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5.0),
+                            borderSide: BorderSide(
+                              color: Colors.grey,
+                              width: 1.0,
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5.0),
+                            borderSide: BorderSide(
+                              color: Colors.grey,
+                              width: 1.0,
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 15.0),
+                      //===== Button submit Login =====
+                      InkWell(
+                        onTap: () {
+                          setState(() {
+                            register();
+                          });
+                        },
+                        child: Container(
+                          padding: EdgeInsets.symmetric(horizontal: 5.0),
+                          height: 55.0,
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                            color: Colors.purple,
+                            borderRadius: BorderRadius.circular(5.0),
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(Icons.lock_open, color: Colors.white),
+                              SizedBox(width: 5.0),
+                              Text(
+                                'Sign up - App Account',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20.0,
+                                  fontWeight: FontWeight.bold,
+                                  wordSpacing: 2.0,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 20.0),
+                //===== Button Link Login =====
+                Row(
+                  children: [
+                    Text(
+                      "Allredy have an account?",
+                      style: TextStyle(fontSize: 18.0, color: Colors.black54),
+                    ),
+                    const SizedBox(width: 10.0),
+                    InkWell(
+                      onTap: () {
+                        setState(() {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const LoginScreen(),
+                            ),
+                          );
+                        });
+                      },
+                      child: Text(
+                        'Sign In',
+                        style: TextStyle(
+                          fontSize: 18.0,
+                          color: Colors.orange,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
